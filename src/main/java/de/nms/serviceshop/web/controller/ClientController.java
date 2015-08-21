7de5fe34a365ef609d28585142c08beadc2a5c5f
Nathan <nmsolis12@gmail.com>
2015-08-20 22:23:45 -0500

    Initial Project Creation

---
+++ src/main/java/de/nms/serviceshop/web/controller/ClientController.java
@@ -0,0 +1,14 @@
+package de.nms.serviceshop.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ClientController {
+
+	@RequestMapping(value = "/clientManager", method = RequestMethod.GET)
+	public String manageClients() {
+		return "clientManager";
+	}
+}
